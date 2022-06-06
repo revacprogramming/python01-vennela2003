c9215422de39ad5649a22caaeb881b093bcf76dd
vennela_sowjanya <94883183+vennela2003@users.noreply.github.com>
2022-06-06 03:33:09 +0000

    executed

--- ActivitySet01/problem02.py
+++ ActivitySet01/problem02.py
@@ -1,3 +1,12 @@
 # Why Program
 
-print("hello world")
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+
+    if  a>=b and a>=c:
+    print(f"{a} is the largest number")
+    elif  b>=c:
+    print(f"{b} id the largest number")
+    else:
+    print(f"{c} is the largest number")
--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,3 +1,7 @@
 # Strings
 
 text = "X-DSPAM-Confidence:    0.8475"
+x = text.find(" ")
+y = text[x:]
+fy=float(y)
+print(fy)
